ceff9c118a0ddc19e75a74269aea5568f8d51618
shubhamdev30 <140228110+shubhamdev30@users.noreply.github.com>
2026-01-14 12:33:28 +0000

    add mogodb uri 14/01

--- server/index.js
+++ server/index.js
@@ -41,7 +41,6 @@ const limiter = rateLimit({
     legacyHeaders: false,
 });
 app.use('/api/', limiter);
-
 // Body Parser Middleware
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
