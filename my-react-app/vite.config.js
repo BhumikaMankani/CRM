1f563561a097a8481391e3ed63c2b27734abec18
bhumika-m-mandasa <bhumika.mankani@mandasadevelopment.com>
2026-08-06 15:11:35 +0530

    CRM Project

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -7,8 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        // target: "http://localhost:5000",
-        target: "https://crm-app-mandasatechnologies.fly.dev",
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
       },
